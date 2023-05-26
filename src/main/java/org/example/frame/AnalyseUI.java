4fcfa15023969f3c91e841d95807a595613a19aa
Langxecho <1908630172@qq.com>
2023-05-26 12:54:58 +0800

    Merge remote-tracking branch 'origin/test' into test
    # Conflicts:
    #	src/main/java/org/example/service/UserService.java
    #	src/test/java/org/example/service/impl/UserServiceImplTest.java
    #	target/classes/org/example/dao/impl/UserDaoImpl.class

--- src/main/java/org/example/frame/AnalyseUI.java
+++ src/main/java/org/example/frame/AnalyseUI.java
@@ -2,6 +2,7 @@ package org.example.frame;
 
 
 
+import org.example.service.impl.AdminServiceImpl;
 import org.example.util.getMiddlelocation;
 
 import javax.swing.*;
@@ -16,14 +17,17 @@ public class AnalyseUI {
     int all;
     int soldnumber;
     double profits;
-JLabel l1 = new JLabel("商品总数量:" + all);
-JLabel l2 = new JLabel("销量:" + soldnumber);
-JLabel l3 = new JLabel("利润:" + profits + "元");
+JLabel l1 = new JLabel();
+JLabel l2 = new JLabel();
+JLabel l3 = new JLabel();
 JButton back = new JButton("返回");
 JButton printbutton = new JButton("打印报表");
 JButton datasave = new JButton("数据备份");
 
     public void startUI(){
+        all = new AdminServiceImpl().goodNumber();
+        soldnumber = new AdminServiceImpl().soldNumber();
+        profits = new AdminServiceImpl().profits();
         JFrame frame = new JFrame("数据分析");
         //这里添加初始化数据的方法
         //frame的基本操作
@@ -35,10 +39,13 @@ JButton datasave = new JButton("数据备份");
         frame.setLayout(null);
         l1.setBounds(45,15,300,60);
         l1.setFont(new Font("SimSong",Font.BOLD,20));
+        l1.setText("商品总数量:" + all);
         l2.setBounds(45,55,300,60);
         l2.setFont(new Font("SimSong",Font.BOLD,20));
+        l2.setText("销量:" + soldnumber);
         l3.setBounds(45,95,300,60);
         l3.setFont(new Font("SimSong",Font.BOLD,20));
+        l3.setText("利润:" + profits + "元");
         frame.add(l1);
         frame.add(l2);
         frame.add(l3);
@@ -51,10 +58,16 @@ JButton datasave = new JButton("数据备份");
             }
         });
         printbutton.setBounds(125,280,100,30);
+        printbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new AdminServiceImpl().print(all,soldnumber,profits);
+            }
+        });
         datasave.setBounds(245,280,100,30);
         frame.add(back);
         frame.add(printbutton);
-        frame.add(datasave);
+//        frame.add(datasave);
         frame.setVisible(true);
     }
 
